ca3a1c8b40120b0256e617e3e5a5c8557a7398c9
trung <107516653+tranquoctrung257@users.noreply.github.com>
2024-01-18 19:08:25 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Cập nhật Termux
-pkg update -y && pkg upgrade -y
-
 # Cấp quyền truy cập vào bộ nhớ
 termux-setup-storage
 
+# Cập nhật Termux
+pkg update -y && pkg upgrade -y
+
 # Cài đặt các ứng dụng cần thiết
 pkg install -y git nano mariadb
+
+# Giao diện
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Kết thúc thông báo
 echo "Cài đặt và cập nhật hoàn tất."
