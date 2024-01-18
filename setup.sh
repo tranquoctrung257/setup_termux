bfb5814c9e67ee5ef0cdabeaa660d15cf5d85bba
trung <107516653+tranquoctrung257@users.noreply.github.com>
2024-01-18 19:36:33 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+# Setup_termux
+# Version    : 0.0
+# Github     : https://github.com/KasRoudra
+# Date       : 18-1-2024
+# Language   : Shell
+
+# color
+
+black="\033[0;30m"
+red="\033[0;31m"
+green="\033[0;32m"
+yellow="\033[0;33m"
+blue="\033[0;34m"
+purple="\033[0;35m"
+cyan="\033[0;36m"
+white="\033[0;37m"
+nc="\033[00m"
+
+version="0.0"
+
+logo="
+${green}████████╗███████╗██████╗░███╗░░░███╗██╗░░░██╗██╗░░██╗
+${blue}╚══██╔══╝██╔════╝██╔══██╗████╗░████║██║░░░██║╚██╗██╔╝
+${red}░░░██║░░░█████╗░░██████╔╝██╔████╔██║██║░░░██║░╚███╔╝░
+${yellow}░░░██║░░░██╔══╝░░██╔══██╗██║╚██╔╝██║██║░░░██║░██╔██╗░
+${cyan} ░░░██║░░░███████╗██║░░██║██║░╚═╝░██║╚██████╔╝██╔╝╚██╗
+${green}░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░╚═════╝░╚═╝░░╚═╝  [v${version}]
+${blue}                                            [By Q_TRUNG]
+${nc}"
+
 # Cấp quyền truy cập vào bộ nhớ
 termux-setup-storage
 
@@ -9,8 +39,13 @@ pkg update -y && pkg upgrade -y
 # Cài đặt các ứng dụng cần thiết
 pkg install -y git nano mariadb
 
+clear
+echo -e "$logo"
+sleep 1
+echo "Cài đặt và cập nhật hoàn tất."
+sleep 3
+
 # Giao diện
 cd /sdcard && mkdir theme_termux && cd theme_termux 
-
-# Kết thúc thông báo
-echo "Cài đặt và cập nhật hoàn tất."
+git clone https://github.com/Sohil876/Termux-zsh.git && cd Termux-zsh
+bash setup.sh
